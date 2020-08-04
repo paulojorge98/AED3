e2ef01728e3f169ffd64ff607b59d4d635365a02
paulojorge98 <paulojorge9812@gmail.com>
2020-08-03 21:15:44 -0300

    Verificação Maior Idade

--- main.cpp
+++ main.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
+#include <tchar.h>
 
 using namespace std;
 
 int main ()
 {
 	string nome;
+	int idade;
 	
+	_tsetlocale(LC_ALL, _T("portuguese"));
 	
-	cout << "Digite seu nome:";
+	cout << "Digite seu nome: ";
 	cin >> nome;
-	cout << "Seja bem vindo " << nome;
+	
+	cout << "Digite sua idade: ";
+	cin >> idade;
+	
+	if (idade >= 18)
+	{
+		cout << nome << " é maior de idade";
+	}
+	
+	else cout << nome << " é menor de idade";
+	
+	
+	
+	
+	
 	return 0;
 }
